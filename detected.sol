ef89f7dd4be9cf569f3a06b29e1400649c4c3e99
listimdn10 <quynhnhu170218@gmail.com>
2025-11-18 20:30:55 +0700

    everything ok

--- detected.sol
+++ detected.sol
@@ -1,153 +1,23 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.4.15;
 
-contract owned {
-    address public owner;
+//Auction susceptible to DoS attack
+contract DosAuction {
+  address currentFrontrunner;
+  uint currentBid;
 
-    constructor() {
-        owner = msg.sender;
+  //Takes in bid, refunding the frontrunner if they are outbid
+  function bid() payable {
+    require(msg.value > currentBid);
+
+    //If the refund fails, the entire transaction reverts.
+    //Therefore a frontrunner who always fails will win
+    if (currentFrontrunner != 0) {
+      //E.g. if recipients fallback function is just revert()
+      // <yes> <report> DENIAL_OF_SERVICE
+      require(currentFrontrunner.send(currentBid));
     }
 
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
-
-    function transferOwnership(address newOwner) onlyOwner public {
-        owner = newOwner;
-    }
-}
-
-interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes memory _extraData) external; }
-
-contract TokenERC20 {
-    
-    string public name;
-    string public symbol;
-    uint8 public decimals = 18;
-    
-    uint256 public totalSupply;
-
-    
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
-
-    
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    
-    event Burn(address indexed from, uint256 value);
-
-
-    constructor(
-        uint256 initialSupply,
-        string memory tokenName,
-        string memory tokenSymbol
-    ) {
-        totalSupply = initialSupply * 10 ** uint256(decimals); 
-        balanceOf[msg.sender] = totalSupply;             
-        name = tokenName;                    
-        symbol = tokenSymbol;              
-    }
-
-
-    function _transfer(address _from, address _to, uint _value) internal virtual {
-        
-        require(_to != address(0));
-        
-        require(balanceOf[_from] >= _value);
-        
-        require(balanceOf[_to] + _value > balanceOf[_to]);
-        
-        uint previousBalances = balanceOf[_from] + balanceOf[_to];
-        
-        balanceOf[_from] -= _value;
-        
-        balanceOf[_to] += _value;
-        emit Transfer(_from, _to, _value);
-        
-        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
-    }
-
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        _transfer(msg.sender, _to, _value);
-        return true;
-    }
-
-
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= allowance[_from][msg.sender]);     
-        allowance[_from][msg.sender] -= _value;
-        _transfer(_from, _to, _value);
-        return true;
-    }
-
-
-    function approve(address _spender, uint256 _value) public
-        returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-        return true;
-    }
-
-
-    function approveAndCall(address _spender, uint256 _value, bytes memory _extraData)
-        public
-        returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, address(this), _extraData);
-            return true;
-        }
-    }
-
-    function burn(uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);  
-        balanceOf[msg.sender] -= _value;      
-        totalSupply -= _value;               
-        emit Burn(msg.sender, _value);
-        return true;
-    }
-
-    function burnFrom(address _from, uint256 _value) public returns (bool success) {
-        require(balanceOf[_from] >= _value);               
-        require(_value <= allowance[_from][msg.sender]);  
-        balanceOf[_from] -= _value;                      
-        allowance[_from][msg.sender] -= _value;    
-        totalSupply -= _value;                  
-        emit Burn(_from, _value);
-        return true;
-    }
-}
-
-contract SETC is owned, TokenERC20 {
-    mapping (address => bool) public frozenAccount;
-    event FrozenFunds(address target, bool frozen);
-    constructor(
-        uint256 initialSupply,
-        string memory tokenName,
-        string memory tokenSymbol
-    ) TokenERC20(initialSupply, tokenName, tokenSymbol) {}
-
-    function _transfer(address _from, address _to, uint _value) internal override {
-        require (_to != address(0));                               
-        require (balanceOf[_from] >= _value);               
-        require (balanceOf[_to] + _value >= balanceOf[_to]); 
-        require(!frozenAccount[_from]);                     
-        require(!frozenAccount[_to]);                       
-        balanceOf[_from] -= _value;                         
-        balanceOf[_to] += _value;                           
-        emit Transfer(_from, _to, _value);
-    }
-
-    function mintToken(address target, uint256 mintedAmount) onlyOwner public {
-        balanceOf[target] += mintedAmount;
-        totalSupply += mintedAmount;
-        emit Transfer(address(0), address(this), mintedAmount);
-        emit Transfer(address(this), target, mintedAmount);
-    }
-
-    function freezeAccount(address target, bool freeze) onlyOwner public {
-        frozenAccount[target] = freeze;
-        emit FrozenFunds(target, freeze);
-    }
-
+    currentFrontrunner = msg.sender;
+    currentBid         = msg.value;
+  }
 }
